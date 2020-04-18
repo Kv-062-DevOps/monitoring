f616043799486e82ecafe5268468b8b8bb45be6f
Khikmet <kerimovkhikmet@gmail.com>
2020-04-18 19:58:25 +0300

    edited output

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	COUNT = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Count = prometheus.NewCounterVec(prometheus.CounterOpts{
 		//Because the Name cannot be duplicate, the recommended rule is: "department Name business Name module Name scalar Name type"
 		Name: "request_count",     //Unique id, can't repeat Register(), can Unregister()
 		Help: "App Request Count", //Description of this Counter
@@ -16,7 +16,7 @@ var (
 	},
 		[]string{"app_name", "method", "endpoint", "http_status"},
 	)
-	LATENCY = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Latency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "request_latency_seconds",
 		Help:    "Request latency",
 		Buckets: prometheus.LinearBuckets(0, 1, 10), //There are 20 first barrels, 5 intervals for each barrel, 5 barrels in total. So 20, 25, 30, 35, 40
@@ -34,8 +34,8 @@ func init() {
 
 	//Cannot register Metrics with the same Name more than once
 	//MustRegister registration failure will directly panic(), if you want to capture error, it is recommended to use Register()
-	prometheus.MustRegister(COUNT)
-	prometheus.MustRegister(LATENCY)
+	prometheus.MustRegister(Count)
+	prometheus.MustRegister(Latency)
 	//prometheus.MustRegister(MyTestSummary)
 
 	// go func() {
@@ -58,10 +58,10 @@ func Timer() {
 	defer timer.ObserveDuration()
 }
 func PostCount() {
-	COUNT.With(prometheus.Labels{"app_name": "post-srv", "method": http.Handle, "endpoint": http.Request, "http_status": http.ResponseWriter})
+	Count.With(prometheus.Labels{"app_name": "post-srv", "method": http.Handle, "endpoint": http.Request, "http_status": http.ResponseWriter})
 }
 func PostHist() {
-	LATENCY.With(prometheus.Labels{"app_name": "post-srv", "endpoint": http.Request})
+	Latency.With(prometheus.Labels{"app_name": "post-srv", "endpoint": http.Request})
 }
 func Output() {
 	http.Handle("/metrics", promhttp.Handler())
