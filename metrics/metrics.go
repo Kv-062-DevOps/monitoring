77f6c50b5683a987eb7e9a822d6c7079ff985f6f
Khikmet <kerimovkhikmet@gmail.com>
2020-04-19 12:41:49 +0300

    removed histogram

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -14,19 +14,28 @@ var (
 	},
 		[]string{"app_name", "method", "endpoint", "http_status"},
 	)
-	Latency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    "request_latency_seconds",
-		Help:    "Request latency",
-		Buckets: prometheus.LinearBuckets(0, 1, 10),
-	},
-		[]string{"app_name", "endpoint"},
-	)
+	// Latency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	// 	Name:    "request_latency_seconds",
+	// 	Help:    "Request latency",
+	// 	Buckets: prometheus.DefBuckets([]float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 7.5, 10}),
+	// },
+	// 	[]string{"app_name", "endpoint"},
+	// )
 )
 
 func init() {
 	prometheus.MustRegister(Count)
-	prometheus.MustRegister(Latency)
+	//prometheus.MustRegister(Latency)
 }
+
+// func StartTimer() {
+// 	http.Request.StartTime = time.Now()
+// }
+
+// func StopTimer() {
+// 	RespTime = time.Now() - http.Request.StartTime
+// 	Latency.
+// }
 
 // func StartTime() {
 // 	var Start = time.Now()
@@ -38,17 +47,17 @@ func init() {
 // }
 
 func PostCount() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		Count.With(prometheus.Labels{"app_name": "post-srv", "method": r.Method,
 			"endpoint": r.Host, "http_status": r.Response.Status}).Inc()
 	})
 }
 
-func PostHist() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		Latency.With(prometheus.Labels{"app_name": "post-srv", "endpoint": r.Host})
-	})
-}
+// func PostHist() {
+// 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+// 		Latency.With(prometheus.Labels{"app_name": "post-srv", "endpoint": r.Host})
+// 	})
+// }
 
 func Output() {
 	http.Handle("/metrics", promhttp.Handler())
