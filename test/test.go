6cc77de43ab58bf628f6f0a2c46a8980255d5d02
Khikmet <kerimovkhikmet@gmail.com>
2020-04-19 18:57:49 +0300

    added test

---
+++ test/test.go
@@ -0,0 +1,79 @@
+package test
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	counter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "golang",
+			Name:      "my_counter",
+			Help:      "This is my counter",
+		})
+
+	histogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: "golang",
+			Name:      "my_histogram",
+			Help:      "This is my histogram",
+		})
+)
+
+func Count() {
+	// rand.Seed(time.Now().Unix())
+
+	prometheus.MustRegister(counter)
+
+	go func() {
+		for {
+			counter.Add(rand.Float64() * 5)
+		}
+	}()
+}
+
+func Hist() {
+	// rand.Seed(time.Now().Unix())
+
+	histogramVec := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "prom_request_time",
+		Help: "Time it has taken to retrieve the metrics",
+	}, []string{"time"})
+
+	prometheus.Register(histogramVec)
+
+	http.Handle("/metrics", newHandlerWithHistogram(promhttp.Handler(), histogramVec))
+
+	prometheus.MustRegister(histogram)
+
+	go func() {
+		for {
+			histogram.Observe(rand.Float64() * 10)
+		}
+	}()
+}
+
+func newHandlerWithHistogram(handler http.Handler, histogram *prometheus.HistogramVec) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		status := http.StatusOK
+
+		defer func() {
+			histogram.WithLabelValues(fmt.Sprintf("%d", status)).Observe(time.Since(start).Seconds())
+		}()
+
+		if req.Method == http.MethodGet {
+			handler.ServeHTTP(w, req)
+			return
+		}
+		status = http.StatusBadRequest
+
+		w.WriteHeader(status)
+	})
+}
