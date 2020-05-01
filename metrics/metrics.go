b9705c2459b75b8ef739f85837fd0ad9831a0c0a
Khikmet <kerimovkhikmet@gmail.com>
2020-05-01 17:51:21 +0300

    edited metrics module

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -8,11 +8,13 @@ import (
 )
 
 var (
+	// CounterVec vector, to count amount of requests
 	CounterVec = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "request_count",
 		Help: "App Request Count",
 	}, []string{"app_name", "method", "endpoint", "http_status"},
 	)
+	// HistogramVec vector, to measure time
 	HistogramVec = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name: "request_latency_seconds",
 		Help: "Request latency",
@@ -20,14 +22,14 @@ var (
 	)
 )
 
-func Count() {
+// RegMetrics func for registering metrics, so they can be exposed
+func RegMetrics() {
 	prometheus.Register(CounterVec)
-}
-
-func HistRegister() {
 	prometheus.Register(HistogramVec)
 }
 
+// Output func which provides handler for exposing metrics via an HTTP server,
+// "/metrics" is the usual endpoint for that
 func Output() {
 	http.Handle("/metrics", promhttp.Handler())
 }
